1d55afc1fd1471847db84d8b16d6ba51362cb969
AbhishekMitra-AIT <abhishekmitra91@gmail.com>
2022-08-19 15:42:49 +0530

    sixth commit

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%1#_wlwp-e$fkgw@h-kvb8ci%hvsz-+fd=k31jipf&^%1i1dr3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','practicewebsite01.herokuapp.com']
 
 
 # Application definition
