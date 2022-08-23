adfdb87d1e5d6f52887b4a08b84a554cb3c2a07f
AbhishekMitra-AIT <abhishekmitra91@gmail.com>
2022-08-23 18:04:02 +0530

    (13th commit added images to staticfiles folder)

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -118,11 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR,'media')
-STATIC_URL = 'staticfiles/'
-
+STATIC_URL = ''
+STATIC_ROOT = ''
 
 MEDIA_URL = ''
 MEDIA_ROOT = ''
